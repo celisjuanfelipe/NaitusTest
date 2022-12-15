a82805c27e7e6710e62e3904cc1147502ec1ceee
celisjuanfelipe <juuan.felipe@gmail.com>
2022-12-14 22:55:56 -0300

    Context

---
+++ Models/railwayContext.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace NaitusTest.Models
+{
+    public partial class railwayContext : DbContext
+    {
+        public railwayContext()
+        {
+        }
+
+        public railwayContext(DbContextOptions<railwayContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<User> Users { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+//                optionsBuilder.UseMySql("server=containers-us-west-139.railway.app;port=6038;database=railway;uid=root;password=XzQCDIuT3WTnzIkwLNHu", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.31-mysql"));
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.UseCollation("utf8mb4_0900_ai_ci")
+                .HasCharSet("utf8mb4");
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.ToTable("users");
+
+                entity.Property(e => e.Id)
+                    .ValueGeneratedNever()
+                    .HasColumnName("id");
+
+                entity.Property(e => e.Date).HasColumnName("date");
+
+                entity.Property(e => e.Email)
+                    .HasColumnType("text")
+                    .HasColumnName("email");
+
+                entity.Property(e => e.LastName)
+                    .HasColumnType("text")
+                    .HasColumnName("lastName");
+
+                entity.Property(e => e.Name)
+                    .HasColumnType("text")
+                    .HasColumnName("name");
+
+                entity.Property(e => e.Pass)
+                    .HasColumnType("text")
+                    .HasColumnName("pass");
+
+                entity.Property(e => e.Rut)
+                    .HasColumnType("text")
+                    .HasColumnName("rut");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
