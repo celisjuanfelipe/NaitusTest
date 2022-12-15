63b07c8f5be911bcb9c9bcba2f59724a708f7c5e
celisjuanfelipe <juuan.felipe@gmail.com>
2022-12-14 22:56:45 -0300

    MVC

---
+++ Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NaitusTest.Models
+{
+    public partial class User
+    {
+        public int Id { get; set; }
+        public string Rut { get; set; } = null!;
+        public string? Name { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+        public DateOnly? Date { get; set; }
+        public string? Pass { get; set; }
+    }
+}
